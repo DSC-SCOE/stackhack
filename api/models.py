0b29c84d5d618812c947514ffa0c16e3ea25cd45
ima.n00b <hemantd983@gmail.com>
2021-01-13 14:09:30 +0530

    creating-models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class empData(models.Model):
+    eid = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True),
+    tenth_marks = models.PositiveIntegerField()
+    tenth_cert = models.FileField()
+    twelth_marks = models.PositiveIntegerField()
+    twelth_cert = models.FileField()
+    degree_marks = models.PositiveIntegerField()
+    degree_cert = models.FileField()
+    resume = models.FileField(blank=True)
+
+
+class leaveModel(models.Model):
+    eid = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True),
+    cur_date = models.DateField()
+    from_date = models.DateField()
+    to_date = models.DateField()
+    readon = models.TextField()

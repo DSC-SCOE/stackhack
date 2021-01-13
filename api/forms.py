5b22559f93f0f5a08f388d3684908cdbf5f040c5
ima.n00b <hemantd983@gmail.com>
2021-01-13 23:11:54 +0530

    added login and signup

--- api/forms.py
+++ api/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm
 
 from api import models as apiModels
-class signupForm(UserCreationForm):
+class signupForm(UserChangeForm):
     
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email','username','password1', 'password2',) 
-        #fields = "__all__"
+        #fields = ('first_name', 'last_name', 'email','username','password1', 'password2',) 
+        fields = "__all__"
         labels = {
             'email' : 'Email',
         }
@@ -17,4 +17,4 @@ class empForm(forms.ModelForm):
 
     class Meta:
         model = apiModels.empData
-        exclude = ['eid', 'status']
+        fields = "__all__"
--- api/models.py
+++ api/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class empData(models.Model):
-    eid = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True),
+    eid = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    address = models.TextField(max_length=200)
     tenth_marks = models.PositiveIntegerField()
     tenth_cert = models.FileField()
     twelth_marks = models.PositiveIntegerField()
@@ -10,11 +11,36 @@ class empData(models.Model):
     degree_marks = models.PositiveIntegerField()
     degree_cert = models.FileField()
     resume = models.FileField(blank=True)
+    status = models.BooleanField(default=False)
+
+
+EMP_ROLE = {
+    (),
+}
+
+class empModel(models.Model):
+    eid = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    role = models.TextField()
+    team = models.TextField()
+    salary = models.PositiveIntegerField()
 
 
 class leaveModel(models.Model):
-    eid = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True),
+    eid = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     cur_date = models.DateField()
     from_date = models.DateField()
     to_date = models.DateField()
-    readon = models.TextField()
+    reason = models.TextField(max_length=100)
+    discription = models.TextField(max_length=1000)
+
+
+A_CHOICES = {
+    ('1', 'present'),
+    ('0', 'absent'),
+}
+class attendanceModel(models.Model):
+    date = models.DateField()
+    eid = models.OneToOneField(User, on_delete=models.CASCADE)
+    status = models.TextField(choices = A_CHOICES)
+
+

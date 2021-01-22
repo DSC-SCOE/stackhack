2403042aa207bdac71db73808afc381a439f978c
ima.n00b <hemantd983@gmail.com>
2021-01-22 21:16:14 +0530

    edited api/admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import empData, empModel, empList
-=======
 from .models import empData, empModel, leaveModel
->>>>>>> master
 
 
 admin.site.register(empData)
 admin.site.register(empModel)
-<<<<<<< HEAD
 admin.site.register(empList)
-=======
 admin.site.register(leaveModel)
->>>>>>> master
